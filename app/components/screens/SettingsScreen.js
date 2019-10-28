77e869ba2f5cd262039e051a4f6acf076cbf7af0
Sergey Gorbachev <sgorbachev@ivi.ru>
2019-10-28 07:39:22 +0300

    Get uri from oauth

--- app/components/screens/SettingsScreen.js
+++ app/components/screens/SettingsScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { ScrollView, Text, Picker } from 'react-native';
+import { ScrollView, Text, Picker, Linking, Button } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_SETTINGS } from '../../queries';
 
+const oauthUrl = 'https://anilist.co/api/v2/oauth/authorize?client_id=2757&response_type=token';
+
 const SettingsScreen = () => {
   const { data, client } = useQuery(GET_SETTINGS);
+  React.useEffect(() => {
+    const urlListener = (url) => {
+      console.log(url);
+    };
+    Linking.addEventListener('url', urlListener);
+    return () => {
+      Linking.removeEventListener('url', urlListener);
+    };
+  }, []);
   return (
     <ScrollView>
       <Text>Title Language</Text>
@@ -18,6 +29,12 @@ const SettingsScreen = () => {
         <Picker.Item label="Romaji (Shingeki no Kyojin)" value="romaji" />
         <Picker.Item label="Native (進撃の巨人)" value="native" />
       </Picker>
+      <Button
+        onPress={() => {
+          Linking.openURL(oauthUrl);
+        }}
+        title="Login with Anilist"
+      />
     </ScrollView>
   );
 };
